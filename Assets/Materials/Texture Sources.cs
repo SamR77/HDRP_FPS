0fbe6202ec3280f03d72dd6e5043c49545ab97ab
Sam Robichaud <Snrobichaud@gmail.com>
2022-10-18 09:46:23 -0300

    Added Wall material and expanded level a bit more

--- Assets/Materials/Texture Sources.cs
+++ Assets/Materials/Texture Sources.cs
@@ -2,7 +2,7 @@
  
 Copper https://free-3dtextureshd.com/download/copper-textures-seamless-bpr-material-high-resolution-free-download-hd-4k/
 
-
+Painted wall - Modified version of https://www.sharetextures.com/textures/wall/painted-wall-3
 
 
 */
